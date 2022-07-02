9137c79d044dc8423acc4ecb1364550dd0ade347
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-07-02 22:38:55 +0300

    Time: 45 ms (50.57%), Space: 13.9 MB (20.32%) - LeetHub

---
+++ 394-decode-string/394-decode-string.py
@@ -0,0 +1,26 @@
+class Solution:
+    def decodeString(self, s: str) -> str:
+        stack=[]
+        size=len(s)
+        output=""
+        for i in range(size):
+            if s[i]=="]":
+                collect=""
+                i=1
+                k=0
+                while stack[-1]!="[":
+                    collect=stack.pop()+collect
+                else:
+                    stack.pop()
+                while  len(stack)!=0 and stack[-1].isdigit():
+                    k+=int(stack.pop())*i
+                    i*=10
+                stack.append(k*collect)
+            else:
+                stack.append(s[i])
+        for val in stack:
+            output+=val
+        return output
+    '''
+   2[4[kl]]
+    '''

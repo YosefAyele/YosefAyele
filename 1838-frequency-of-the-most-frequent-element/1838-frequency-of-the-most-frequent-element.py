e71ead07ec975ed3d6e8ab3002440a1e66f4dc05
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-08-18 20:30:54 +0300

    Time: 1619 ms (89.39%), Space: 28.6 MB (12.68%) - LeetHub

---
+++ 1838-frequency-of-the-most-frequent-element/1838-frequency-of-the-most-frequent-element.py
@@ -0,0 +1,40 @@
+class Solution:
+    def maxFrequency(self, nums: List[int], k: int) -> int:
+        
+        # '''...Brute Force Solution...'''
+#         maxfreq=0
+#         nums.sort(reverse=True)
+
+#         for i in range(len(nums)):
+#             j=i
+#             oprtn=k
+#             currfreq=0
+#             while j<len(nums) and oprtn-(nums[i]-nums[j])>=0 :
+#                 oprtn-=(nums[i]-nums[j])
+#                 j+=1
+#                 currfreq+=1
+                
+#             maxfreq=max(currfreq,maxfreq)
+        
+#         return maxfreq
+    
+      # '''....Sliding Window Soluiton...'''
+        nums.sort()
+        l=r=0
+        wnlth=0
+        Total=0
+
+        while r<len(nums):
+
+            Total+=nums[r]
+            
+            while nums[r]*(r-l+1)>Total+k:
+                Total-=nums[l]
+                l+=1
+            wnlth=max(r-l+1,wnlth)
+                
+            r+=1
+        return wnlth
+
+
+            

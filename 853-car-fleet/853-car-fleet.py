3bcd0f03d476a2275f9898998382cf977d743220
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-08-21 18:10:24 +0300

    Time: 1190 ms (77.00%), Space: 33.5 MB (89.14%) - LeetHub

---
+++ 853-car-fleet/853-car-fleet.py
@@ -0,0 +1,21 @@
+class Solution:
+    def carFleet(self, tar: int, position: List[int], sp: List[int]) -> int:
+        pos=[]
+        for i in range(len(position)):
+            pos.append([position[i],sp[i]])
+        pos.sort(reverse=True)
+#         print(pos)
+        monstack=[]
+        
+        for pair in pos:
+            
+            currtime=(tar-pair[0])/pair[1]
+            # print(monstack)
+            
+            while monstack and currtime<=monstack[-1]:
+                currtime=monstack.pop()
+            
+            monstack.append(currtime)
+        return len(monstack)
+            
+        

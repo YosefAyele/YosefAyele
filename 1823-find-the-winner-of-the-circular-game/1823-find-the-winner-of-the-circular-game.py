5395f3ea1b4f2f1d953021f3472283e7091adf80
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-08-25 10:39:27 +0300

    Time: 294 ms (25.42%), Space: 13.8 MB (98.66%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,15 +1,23 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         
-        Q=[i for i in range(1,n+1)]
-        r=-1
-        
-        while len(Q)>1:
-            r=(r+k)%len(Q)
-            Q.pop(r)
-            r-=1
+#         Q=[i for i in range(1,n+1)]
+#         r=-1
+#        #solution 1 
+#         while len(Q)>1:
+#             r=(r+k)%len(Q)
+#             Q.pop(r)
+#             r-=1
             
-        return Q[0]
+#         return Q[0]
         
+        #solution2--------Using circular Queue
+        Friends=deque(i for i in range(1,n+1))
+        while len(Friends)>1:
+            for i in range(k-1):
+                Friends.append(Friends.popleft())
+            Friends.popleft()
+        return Friends[0]
+                
             
             

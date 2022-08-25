0f963ea94a1977c4f1d24e9dad462fea31391c82
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-08-25 17:31:19 +0300

    Time: 46 ms (82.96%), Space: 14.3 MB (16.81%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,6 +1,7 @@
 class Solution:
     def findTheWinner(self, n: int, k: int) -> int:
         
+        
 #         Q=[i for i in range(1,n+1)]
 #         r=-1
 #        #solution 1 
@@ -12,12 +13,19 @@ class Solution:
 #         return Q[0]
         
         #solution2--------Using circular Queue
-        Friends=deque(i for i in range(1,n+1))
-        while len(Friends)>1:
-            for i in range(k-1):
-                Friends.append(Friends.popleft())
-            Friends.popleft()
-        return Friends[0]
-                
+        # Friends=deque(i for i in range(1,n+1))
+        # while len(Friends)>1:
+        #     for i in range(k-1):
+        #         Friends.append(Friends.popleft())
+        #     Friends.popleft()
+        # return Friends[0]
+# Recursive Solution
+        def recur(n,k):
+            if n==1:
+                return 0
+            return (recur(n-1,k)+k)%n
+        
+        return recur(n,k)+1
+        
             
             

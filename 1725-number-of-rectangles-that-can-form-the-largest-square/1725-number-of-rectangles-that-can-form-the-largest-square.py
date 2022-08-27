b9f4e666a6d48a68d675cfad5b8a433c13193521
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-08-27 15:15:04 +0300

    Time: 278 ms (62.75%), Space: 15.3 MB (27.57%) - LeetHub

--- 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.py
+++ 1725-number-of-rectangles-that-can-form-the-largest-square/1725-number-of-rectangles-that-can-form-the-largest-square.py
@@ -1,11 +1,32 @@
 class Solution:
     def countGoodRectangles(self, rectangles: List[List[int]]) -> int:
         
-        squares=[]
+#         squares=0
         
-        for i in rectangles:
-            squares.append(min(i[0],i[1]))
+#         for i in rectangles:
+#             squares.append(min(i[0],i[1]))
             
-        return collections.Counter(squares)[max(squares)]
+#         return collections.Counter(squares)[max(squares)]
+        count=0
+        def recur(rect,i,maxval):
+            
+            global count
+            
+            if i==len(rect):
+                return count
+            
+            minm=min(rect[i][0],rect[i][1])
+            
+            if minm>maxval:
+
+                count=1
+            
+                maxval=minm
+            elif minm==maxval:
+                
+                count+=1
+            return recur(rect,i+1,maxval)
+             
         
-       
+        
+        return   recur(rectangles,0,0)

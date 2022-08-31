13de39c2b663786b8b7c5a1939ab1be0310132dd
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-08-31 20:38:26 +0300

    Time: 51 ms (93.92%), Space: 14.2 MB (57.12%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.py
+++ 496-next-greater-element-i/496-next-greater-element-i.py
@@ -1,31 +1,14 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        output=[]
-        # for i in range(len(nums1)):
-        #     for j in range(len(nums2)):
-        #         if nums2[j]==nums1[i]:
-        #             n=j
-        #             break
-        #     k=n+1
-        #     while k<len(nums2) :
-        #         if nums2[k]>nums1[i]:
-        #             ans.append(nums2[k])
-        #             break  
-        #         k+=1
-        #     else:
-        #         ans.append(-1)
-        # return ans
-                    
-        d={n:-1 for n in nums2}
-        minStack = []
-        for num in nums2:
-            while minStack and num > minStack[-1]:
-                popped = minStack.pop()
-                d[popped] = num
-            minStack.append(num)
-        for val in nums1:
-            output.append(d[val])
-        return output
         
-                
+        mapp={i:-1 for i in nums1}
+           
+        monstack=[]
         
+        for i in nums2:
+            
+            while monstack and i>monstack[-1]:
+                mapp[monstack[-1]]=i
+                monstack.pop()
+            monstack.append(i)
+        return [mapp[i] for i in nums1]

92d097cee13998985ec311720d34044101b20602
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-09-01 17:09:30 +0300

    Time: 61 ms (95.42%), Space: 14.1 MB (13.93%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -3,14 +3,23 @@ class Solution:
         
         res=0
         window=""
-
+    
+        # for r in range(len(s)):
+        #     res=max(res,len(window))
+        #     while s[r] in window:
+        #         window=window[1:]
+        #     window+=s[r]
+        # res=max(res,len(window))
+        # return res
+        l=0
+        
         for r in range(len(s)):
-            res=max(res,len(window))
+            
             while s[r] in window:
                 window=window[1:]
+                l+=1
+            res=max(res,r-l+1)
             window+=s[r]
-        res=max(res,len(window))
         return res
-                
             
             

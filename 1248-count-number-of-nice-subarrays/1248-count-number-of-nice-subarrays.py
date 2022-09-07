07fe866a123bf8ef7d1c8f3dc3e6c801e413bf86
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-09-07 11:15:31 +0300

    Time: 910 ms (89.39%), Space: 21.5 MB (23.94%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.py
@@ -1,16 +1,20 @@
-from collections import deque
-
 class Solution:
     def numberOfSubarrays(self, nums: List[int], k: int) -> int:
-        odds=deque()
         res=0
-        odds.append(-1)
+        count=0
+        oddMap={0:1} # mapping count of odd to number of times it occured
         
-        for i in range(len(nums)):
+        for num in nums:
             
-            if nums[i]%2: odds.append(i)
-            if len(odds)>k+1: odds.popleft()
-            if len(odds)==k+1: res=res+odds[1]-odds[0]
-                
+            if num%2:
+                count+=1
+            if count-k in oddMap: 
+                res+=oddMap[count-k]
+            oddMap[count]=oddMap.get(count,0)+1
+            
         return res
         
+        
+        
+        
+  

ee76fa85741cee15d84b85d80fa8be951f459220
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-09-26 12:31:30 +0300

    Time: 61 ms (36.84%), Space: 14.4 MB (14.37%) - LeetHub

---
+++ 273-integer-to-english-words/273-integer-to-english-words.py
@@ -0,0 +1,19 @@
+class Solution:
+    def numberToWords(self, num: int) -> str:
+        num2w={1000000000:"Billion", 1000000:"Million", 1000:"Thousand",100:"Hundred",90:"Ninety",                  80:"Eighty", 70:"Seventy", 60:"Sixty", 50:"Fifty", 40:"Forty", 30:"Thirty",                          20:"Twenty", 19:"Nineteen", 18:"Eighteen", 17:"Seventeen", 16:"Sixteen", 15:"Fifteen",                14:"Fourteen", 13:"Thirteen", 12:"Twelve", 11:"Eleven", 10:"Ten", 9:"Nine",                          8:"Eight",7:"Seven", 6:"Six", 5:"Five",4:"Four", 3:"Three", 2:"Two", 1:"One",0:"Zero"}
+        
+        def helper(num):
+            
+            if num<=20: return num2w[num]
+            
+            for key in num2w:
+                quo, rem= num//key, num%key
+                
+                if not quo: continue
+                    
+                w1= helper(quo)+ " " if key>=100 else ""
+                w2= " "+helper(rem) if rem else ""
+                
+                return w1+num2w[key]+w2
+            
+        return helper(num)

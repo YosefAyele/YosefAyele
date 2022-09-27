c0922c1f27ed905ffd99778a7498e4907d5dec58
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-09-27 20:31:06 +0300

    Time: 1598 ms (19.34%), Space: 36.4 MB (90.33%) - LeetHub

--- 148-sort-list/148-sort-list.py
+++ 148-sort-list/148-sort-list.py
@@ -6,20 +6,44 @@
 class Solution:
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
         
-        temp=head
-        res=[]
+        def findMid(head):
+            
+            slow=None
+            while head and head.next:
+                slow=head if not slow else slow.next
+                head=head.next.next
+                
+            
+            secondhalf=slow.next
+            slow.next=None
+            return secondhalf
         
-        dummy=ListNode()
-        trace=dummy
-        while temp:
-            res.append(temp.val)
-            temp=temp.next
+        def merge(h1,h2):
+            res=ListNode()
+            restail=res
+
+            while h1 and h2:
+
+                if h1.val<=h2.val:
+                    restail.next=h1
+                    h1=h1.next
+
+                else:
+                    restail.next=h2
+                    h2=h2.next
+
+                restail=restail.next
+
+            if h1 or h2:
+                restail.next=h1 if h1 else h2
+
+            return res.next
         
-        res.sort()
-        
-        for val in res:
-            node=ListNode(val)
-            trace.next=node
-            trace=trace.next
-        return dummy.next
-        
+        def sort(head):
+            if not head or not head.next: return head
+            mid=findMid(head)
+            left=sort(head)
+            right=sort(mid)
+            
+            return merge(left,right)
+        return sort(head)

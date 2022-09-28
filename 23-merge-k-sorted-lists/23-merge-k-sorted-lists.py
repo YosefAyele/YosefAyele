44ce5c8aabc452f07fa874b89cf2a8d85d014465
YosefAyele <105995267+YosefAyele@users.noreply.github.com>
2022-09-28 17:39:39 +0300

    Time: 249 ms (26.75%), Space: 17.6 MB (74.23%) - LeetHub

--- 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
+++ 23-merge-k-sorted-lists/23-merge-k-sorted-lists.py
@@ -7,27 +7,36 @@ class Solution:
     def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
         
         def merge(h1,h2):
-            res=ListNode(0)
-            restail=res
-            while h1 and h2:
-                
-                if h1.val<=h2.val:
-                    restail.next=h1
-                    h1=h1.next
-                else:
-                    restail.next=h2
-                    h2=h2.next
-                
-                restail=restail.next
-            if h1 or h2:
-                 restail.next=h1 if h1 else h2
-                
-            return res.next
-        srtd=None
-        for i in range(len(lists)):
-            if not lists[i]: continue
-            srtd=merge(srtd,lists[i])
+                res=ListNode(0)
+                restail=res
+                while h1 and h2:
+
+                    if h1.val<=h2.val:
+                        restail.next=h1
+                        h1=h1.next
+                    else:
+                        restail.next=h2
+                        h2=h2.next
+
+                    restail=restail.next
+                if h1 or h2:
+                     restail.next=h1 if h1 else h2
+
+                return res.next
         
-        return srtd
+        if len(lists)==0: return None
+        
+        while len(lists)>1:
+            merged=[]
+            for i in range(0,len(lists),2):
+                l1=lists[i]
+                l2=lists[i+1] if i+1<len(lists) else None
+                
+                merged.append(merge(l1,l2))
+            lists=merged
+        return lists[0]
+        
+  
+    
         
         
